53c198980f7246fe34f02481b31caa38df2c0925
josephsy96 <josephsy96@hotmail.com>
2019-10-26 18:39:30 -0500

    add files

--- PyBanK_Challenge.py
+++ PyBanK_Challenge.py
@@ -14,28 +14,28 @@ os.chdir(pull_folder)
 #This is to set 
 pybank = os.path.join('../Resources', 'budget_data.csv')
 
-net = []
 
+#list created to compute later functions
+net = []
+date = []
 with open(pybank, newline="") as bank_out:
     net_header = next(bank_out)
     bank_read = csv.reader(bank_out, delimiter = ',')
-    net_total = 0     
-    #average_change = 0.0
-    row_count = 0
-    net.append(bank_out[1])
-        #row_count = sum(1 for row in bank_read)
-#This section of code is to calculate the total net amount of the csv file.      
-    for money_row in bank_read:
-        net_total += int(money_row[1])
-        row_count += 1
-        #net.append(net_total)
+    #net_total = 0
 
-    #get it out of the loop 
-    average_change = net
+    for csv_row in bank_read:
+        net.append(csv_row[1])
+        date.append(csv_row[0])
+        net_total = sum(map(int, net))
+        row_count = len(net)
 
+    sub = int(net[-1]) - int(net[0])
+    average_change = round(sub/(row_count - 1),2)
+
+#output final product    
     print("Financial Analysis")
     print("---" * 10)
     print(f"Total Months: {row_count}")
     print(f"Net Total: ${net_total}")
-    print(f"Average Change:{average_change}")
+    print(f"Average Change:${average_change}")
     print("---" * 10)
