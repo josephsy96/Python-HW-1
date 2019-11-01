29b6a19e2a8ffef069753a4b3dcaf3664ff9e1ef
josephsy96 <josephsy96@hotmail.com>
2019-10-31 21:24:46 -0500

    add files

--- Polling.py
+++ Polling.py
@@ -12,8 +12,9 @@ os.chdir(pull_folder)
 pypoll = os.path.join('../Resources', 'election_data.csv')
 #==================================================
 #creating lists
-vote = []
+vote_list = []
 politics = []
+ballot_num = {}
 #==================================================
 #reading the csv file
 with open(pypoll, newline="") as outfile:
@@ -22,20 +23,50 @@ with open(pypoll, newline="") as outfile:
 
 #==================================================
     for row in poll_read:
-        vote.append(row[0])
+        vote_list.append(row[0])
         politics.append(row[2])
-for vote_index in range(len(vote)):
-    voting = int(vote[vote_index])
-    election = str(politics[vote_index])
-
-
-total_votes = len(vote)
-
+#==================================================
+#To create a dictionary for politicians' name and total votes
+for name in politics:
+    if name not in ballot_num:
+        ballot_num[name] = 1
+    else:
+        ballot_num[name] += 1
+#==================================================
+#variables for total votes and winner totals
+total_votes = len(vote_list)
+winner_total = max(ballot_num.values())
+#finding the winner
+for winner in ballot_num:
+    if ballot_num[winner] == winner_total:
+        winner_name = winner
+#==================================================
+#Print output
 print("Election Results")
 print("---" * 10)
 print(f"Total Votes: {total_votes}")
 print("---" * 10)
-print(f"{election} {voting}")
+#==================================================
+#create an iterating output for candidates and votes
+for key, value in ballot_num.items():
+    print(f"{key}:  {round(((value/total_votes)* 100.00),3)}%  ({value})")
+#==================================================
 
-
+print("---" * 10)
+print(f"Winner: {winner_name}")
+print("---" * 10)
+#==================================================
+#output to txt file
+with open("output.txt", 'x') as out:
+    print("Election Results",file=out)
+    print("---" * 10,file=out)
+    print(f"Total Votes: {total_votes}", file=out)
+    print("---" * 10, file=out)
+#==================================================
+    for key, value in ballot_num.items():
+        print(f"{key}:  {round(((value/total_votes)* 100.00),3)}%  ({value})", file=out)
+#==================================================
+    print("---" * 10, file=out)
+    print(f"Winner: {winner_name}", file = out)
+    print("---" * 10, file = out)
 
